7089c6f447f724472a9344f97447f27315d6c8ed
Tom BRAZZALOTTO <tom.brazzalotto@viacesi.fr>
2018-04-26 23:14:57 +0200

    Mise en place de la base WPF avec Métro. Il manque l'implémentation de MVVM Lite

---
+++ Umbrella-Corps/Umbrella-Corps/MainWindow.xaml.cs
@@ -0,0 +1,14 @@
+﻿using MahApps.Metro.Controls;
+
+namespace Umbrella_Corps
+{
+    /// <summary>
+    /// Logique d'interaction pour MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : MetroWindow
+    {
+        public MainWindow() {
+            InitializeComponent();
+        }
+    }
+}
