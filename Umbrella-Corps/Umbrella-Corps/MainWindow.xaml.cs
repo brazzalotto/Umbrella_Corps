8ebfae07324d37d3d3314e508f568742bee853e1
Fixe <Fixe@DESKTOP-ANERHP7>
2018-08-11 23:11:03 +0200

    ihm ok

--- Umbrella-Corps/Umbrella-Corps/MainWindow.xaml.cs
+++ Umbrella-Corps/Umbrella-Corps/MainWindow.xaml.cs
@@ -1,7 +1,9 @@
 ﻿using MahApps.Metro.Controls;
+using Microsoft.Win32;
 using System;
+using System.Diagnostics;
+using System.IO;
 using System.Windows;
-
 namespace Umbrella_Corps
 {
     /// <summary>
@@ -9,18 +11,59 @@ namespace Umbrella_Corps
     /// </summary>
     public partial class MainWindow : MetroWindow
     {
-        public MainWindow() {
+        string filename;
+        public MainWindow()
+        {
             InitializeComponent();
             var nbHearts = getHeartsProcessor();
-
-
-            MessageBox.Show("Nombre de coeurs : "+ nbHearts + "");
+            MessageBox.Show("Nombre de coeurs : " + nbHearts + "");
+            //MessageBox.Show("Nombre de coeurs : "+ nbHearts + "");
         }
-
         // Récupère de nombre de coeurs sur le processeur
-        public int getHeartsProcessor() {
+        public int getHeartsProcessor()
+        {
             var nbHearts = Environment.ProcessorCount;
             return nbHearts;
+        }
+        private void TextBox_TextChanged(object sender, System.Windows.Controls.TextChangedEventArgs e)
+        {
+        }
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            Microsoft.Win32.OpenFileDialog dlg = new Microsoft.Win32.OpenFileDialog();
+            // Set filter for file extension and default file extension
+            dlg.DefaultExt = ".txt";
+            dlg.Filter = "Text documents (.txt)|*.txt";
+            // Display OpenFileDialog by calling ShowDialog method
+            Nullable<bool> result = dlg.ShowDialog();
+            // Get the selected file name and display in a TextBox
+            if (result == true)
+            {
+                // Open document
+                filename = dlg.FileName;
+                parcourir.Text = filename;
+            }
+        }
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            String mode = modes.Text;
+            MessageBox.Show(filename);
+            StreamReader sr = new StreamReader(filename);
+            try
+            {
+                resultats.Text = sr.ReadToEnd();
+                Console.WriteLine(sr);
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("Le path ne pas pas être vide");
+            }
+            resultats.Text = File.ReadAllText(filename);
+
+            var path = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "/ConsoleApp1/yourprogram.exe");
+            Process.Start(new ProcessStartInfo(path));
+            Process.Start(ConsoleApp1);
+
         }
     }
 }
