b1e8888f6dd3325c5cf69195fae5ee5777b981c0
Tom BRAZZALOTTO <brazzalotto32@gmail.com>
2018-08-20 19:55:14 +0200

    comit test

---
+++ Umbrella-Corps/Umbrella-Corps/MainWindow.xaml.cs
@@ -0,0 +1,26 @@
+﻿using MahApps.Metro.Controls;
+using System;
+using System.Windows;
+
+namespace Umbrella_Corps
+{
+    /// <summary>
+    /// Logique d'interaction pour MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : MetroWindow
+    {
+        public MainWindow() {
+            InitializeComponent();
+            var nbHearts = getHeartsProcessor();
+
+
+            MessageBox.Show("Nombre de coeurs : "+ nbHearts + "");
+        }
+
+        // Récupère de nombre de coeurs sur le processeur
+        public int getHeartsProcessor() {
+            var nbHearts = Environment.ProcessorCount;
+            return nbHearts;
+        }
+    }
+}
