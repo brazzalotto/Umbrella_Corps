73a717c8da194c62104d6b113f187bf4f88ab52e
Thomas CAZALS <thomas.cazals@viacesi.fr>
2018-09-02 19:01:15 +0200

    tcp en cours

---
+++ Umbrella-Corps/PartageTCP/Enum/StatusEnum.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PartageTCP.Enum
+{
+
+        [Serializable]
+        public enum StatusEnum
+        {
+            Connected,
+            Disconnected,
+            Validated,
+            InSession
+        }
+
+}
---
+++ Umbrella-Corps/PartageTCP/Models/ResponseCallbackObject.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+public class ResponseCallbackObject
+{
+    public Delegate CallBack { get; set; }
+    public Guid ID { get; set; }
+}
